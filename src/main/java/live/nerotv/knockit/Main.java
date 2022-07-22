15d4e69ee7fdbfc03b66e78d93dc1b7c8889051d
nerotvlive <daniel.niesmann@icloud.com>
2022-07-22 15:52:05 +0200

    2.14.0-1.19 (Updated to ZyneonChatEvent)

--- src/main/java/live/nerotv/knockit/Main.java
+++ src/main/java/live/nerotv/knockit/Main.java
@@ -11,7 +11,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scoreboard.Team;
 
 public class Main extends JavaPlugin {
 
@@ -129,10 +128,10 @@ public class Main extends JavaPlugin {
             Scoreboard.registerNewTeam("01Creator");
             Scoreboard.registerNewTeam("02Premium");
             Scoreboard.registerNewTeam("03Spieler");
-            Scoreboard.getTeam("00000Team").setPrefix("§9Team §8● §f");
-            Scoreboard.getTeam("01Creator").setPrefix("§9Creator §8● §f");
-            Scoreboard.getTeam("02Premium").setPrefix("§9Premium §8● §f");
-            Scoreboard.getTeam("03Spieler").setPrefix("§9User §8● §f");
+            Scoreboard.getTeam("00000Team").setPrefix("§cTeam §8● §f");
+            Scoreboard.getTeam("01Creator").setPrefix("§dCreator §8● §f");
+            Scoreboard.getTeam("02Premium").setPrefix("§6Premium §8● §f");
+            Scoreboard.getTeam("03Spieler").setPrefix("§7User §8● §f");
             Scoreboard.getTeam("00000Team").setCanSeeFriendlyInvisibles(false);
             Scoreboard.getTeam("01Creator").setCanSeeFriendlyInvisibles(false);
             Scoreboard.getTeam("02Premium").setCanSeeFriendlyInvisibles(false);
